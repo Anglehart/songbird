4e4963d53c2369e78bf8386beb522627c4333a16
Boris Kobzarev <planez@bk.ru>
2020-08-17 01:04:56 +0300

    feat: add bird info (right block)

--- songbird/src/App.js
+++ songbird/src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import Header from './header';
 import BirdsList from './birdsList';
+import BirdInfo from './birdInfo';
 import birdsData from './assets/birdsData';
 import './index.css';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userChoice: 0,
+    };
+    this.correctId = this.getRandomNumber(birdsData[0].length);
+  }
   
   getRandomNumber(max) {
     return Math.floor(Math.random() * max) + 1;
   }
-
+  
+  handleClick(id) {
+    this.setState({userChoice: id});
+  }
+  
   render() {
-    const correctId = this.getRandomNumber(birdsData[0].length);
+    const userChoice = this.state.userChoice;
     return (
       <div className="wrapper">
         <Header />
         <div className="second-row">
-          <BirdsList birdsData={birdsData[0]} onChoice={() => this.handleClick()} correctId={correctId} />
+          <BirdsList birdsData={birdsData[0]} onChoice={this.handleClick.bind(this)} />
+          <BirdInfo birdsData={birdsData[0]} userChoice={userChoice} />
         </div>
       </div>
     );
